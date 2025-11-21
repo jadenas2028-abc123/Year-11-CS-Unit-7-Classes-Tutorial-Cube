f732cbc07d68ff00347f8f1cc75f1a2d11081534
Jaden See <jadenas2028@student.cis.edu.hk>
2025-11-21 15:10:32 +0800

    Cube 1 21/11/2025

--- src/Cube.java
+++ src/Cube.java
@@ -1,23 +1,32 @@
 public class Cube {
     private int side;
 
+    public String toString(){
+        return "Cube{side=" + side + "}";
+    }
+
     public int getSide(){
         return side;
     }
 
     public void setSide(int side){
+        if (side < 1) {
+            throw new IllegalArgumentException("A cube's side length cannot be less than 1");
+        } else
         this.side=side;
     }
 
     public Cube (int side){
         if (side < 1) {
-            throw new IllegalArgumentException("A cube’s side length must be equal to or greater than 1!");
+            throw new IllegalArgumentException("A cube's side length cannot be less than 1!");
         }
         this.side=side;
     }
+
     public Cube(){
         side = 1;
     }
+
     public int calculateVolume(){
         return side * side * side;
     }
